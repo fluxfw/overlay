37354d6c908b91c2a4ce340d9236eabcb95b2b2d
fluxfw <fluxfw@fluxlabs.ch>
2023-04-14 15:15:16 +0200

    Fix

--- src/Input.mjs
+++ src/Input.mjs
@@ -1,5 +1,5 @@
 /** @typedef {import("./Option.mjs").Option} Option */
 
 /**
- * @typedef {{"auto-focus"?: boolean, disabled?: boolean, "input-mode"?: string, label?: string, max?: string, "max-length"?: number, min?: string, "min-length"?: number, multiple?: boolean, name: string, options?: Option[], pattern?: string, placeholder?: string, "read-only"?: boolean, required?: boolean, step?: string, title?: string, type?: "color" | "date" | "datetime-local" | "email" | "month" | "number" | "password" | "search" | "select" | "tel" | "text" | "textarea" | "time" | "url" | "week", value?: string | number | null}} Input
+ * @typedef {{disabled?: boolean, "input-mode"?: string, label?: string, max?: string, "max-length"?: number, min?: string, "min-length"?: number, multiple?: boolean, name: string, options?: Option[], pattern?: string, placeholder?: string, "read-only"?: boolean, required?: boolean, step?: string, title?: string, type?: "color" | "date" | "datetime-local" | "email" | "month" | "number" | "password" | "search" | "select" | "tel" | "text" | "textarea" | "time" | "url" | "week", value?: string | number | null}} Input
  */
